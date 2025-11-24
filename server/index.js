a731c21e08368638b10822411cf695bdb3f8b805
tsitu0 <tomsitu0102@gmail.com>
2025-11-24 12:33:49 -0800

    clean up

--- server/index.js
+++ server/index.js
@@ -1,40 +1,13 @@
-/*
-Importing express library
-*/
 const express = require('express'); 
 
-/*
-Creates an express "app"
-This is the server
-Will add all the routes and logic onto here
-*/
 const app = express();
 
-/*
-Middleware to parse JSON body (for POST later)
-so express is able to handle json bodies
-*/
 app.use(express.json());
 
-/*
-link main server to routes folder
-*/
 const slotsRouter = require('./routes/slots');
-/*
-switch to api bc industry standard, app.use any req
-that starts with this path should be handles by this router
-*/
 app.use('/api/slots', slotsRouter);
 
-/*
-app.listen() turn the server ON and tell it to listen for
-incoming requests
 
-listen(port, callback)
-
-“What door?” → the port
-“What to do when open?” → callback function
-*/
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
--- server/routes/slots.js
+++ server/routes/slots.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-// Temp database
+//Temporary database, will convert to mongodb later
 let slots = [
   { id: 1, time: "10:00 AM", reserved: false, candidateName: null, checkedIn:false },
   { id: 2, time: "11:00 AM", reserved: false, candidateName: null, checkedIn:false },
   { id: 3, time: "1:00 PM", reserved: true, candidateName: "Tom" , checkedIn:false }
 ];
 
-//    http://localhost:5000/api/slots
+// http://localhost:5000/api/slots
 router.get('/', (req, res) => {
   res.json(slots)
 });
 
-//    http://localhost:5000/api/slots/:id
+// http://localhost:5000/api/slots/:id
 router.get('/:id', (req, res) => {
   const slotID = parseInt(req.params.id);
   const slot = slots.find(s => s.id == slotID);
@@ -25,40 +25,29 @@ router.get('/:id', (req, res) => {
   res.json(slot);
 });
 
-//    http://localhost:5000/api/slots/reserve/:id
+// http://localhost:5000/api/slots/reserve/:id
 router.post('/reserve/:id', (req, res) => {
   const slotID = parseInt(req.params.id);
-
-  //returns a slots object if a match
   const slot = slots.find(s => s.id == slotID);
-
-  //if the object doesnt exist
   if(!slot){
     return res.status(404).json({ error: "Slot not found" });
   }
-
   if(slot.reserved){
     return res.status(400).json({ error: "Slot already reserved" });
   }
-  //body instead of param theres a difference
   const name = req.body.candidateName;
-  //if the name isnt provided
   if (!name) {
     return res.status(400).json({ error: "Candidate name is required" });
   }
-
-  //update the slot
   slot.reserved = true;
   slot.candidateName = name;
-
   res.json({
     message: "Slot reserved successfully",
     slot
   });
-
 });
 
-//    http://localhost:5000/api/slots/checkin/:id
+// http://localhost:5000/api/slots/checkin/:id
 router.post("/checkin/:id", (req, res) => {
   const slotID = parseInt(req.params.id);
   const slot = slots.find(s => s.id == slotID);
@@ -82,7 +71,7 @@ router.post("/checkin/:id", (req, res) => {
 
 })
 
-//    http://localhost:5000/api/slots/cancel/:id
+// http://localhost:5000/api/slots/cancel/:id
 router.post('/cancel/:id', (req, res) => {
   const slotID = parseInt(req.params.id);
   const slot = slots.find(s => s.id == slotID);
